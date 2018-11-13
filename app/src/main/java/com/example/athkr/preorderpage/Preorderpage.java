28129da3d5ad738432c980de8764bae8b697ed1b
TAAAE <athkrit_tae@hotmail.com>
2018-11-13 16:22:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/athkr/preorderpage/Preorderpage.java
@@ -0,0 +1,13 @@
+package com.example.athkr.preorderpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Preorderpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preorderpage);
+    }
+}
